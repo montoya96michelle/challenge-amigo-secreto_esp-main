d0ece132ee29216d0946a6d5cd42d02f71475648
michelle.96 <michelle96montoya@gmail.com>
2025-08-08 22:32:06 -0500

    Agregando funcion para limpiar entrada

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
     if (nombre === "" ||  Number(nombre)) {
         alert("Por favor, inserte un nombre.");
+        limpiarCampo();
         return;
     } else {
         amigos.push(nombre);
+        limpiarCampo();
         console.log(amigos);
     }
+}
+
+// borrar el texto del campo de entrada
+function limpiarCampo() {
+    document.getElementById('amigo').value = "";
 }
